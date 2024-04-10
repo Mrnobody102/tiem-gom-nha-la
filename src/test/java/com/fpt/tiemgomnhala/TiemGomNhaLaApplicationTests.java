535537e4f69782cbd6446b80fd1b68ec87ee81f8
Pham Quang Huy (k15 HL) <huypq1801@gmail.com>
2024-04-10 15:31:02 +0700

    create project

---
+++ src/test/java/com/fpt/tiemgomnhala/TiemGomNhaLaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.tiemgomnhala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiemGomNhaLaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
